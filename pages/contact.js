12302397ef51e57c0fb795f1a747862b93769da3
maria.edwards <maria.ana.edwards@gmail.com>
2023-04-05 15:59:11 -0700

    Adds contact with FAKE mogodb connection

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,18 @@
-export default function ContactPage() {
-  return <>Contact page</>;
+import { Fragment } from 'react';
+import Head from 'next/head';
+
+import ContactForm from '../components/contact/ContactForm';
+
+function ContactPage() {
+  return (
+    <>
+      <Head>
+        <title>Contact Me</title>
+        <meta name="description" content="Send me your messages!" />
+      </Head>
+      <ContactForm />
+    </>
+  );
 }
+
+export default ContactPage;
